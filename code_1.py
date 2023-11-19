0918665892ffe8fbdea7d40021ee96a8f8cf908a
EgarJ <141117607+EgarJ@users.noreply.github.com>
2023-11-19 11:20:31 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,39 +1,27 @@
-    return data
-    main()
-def generate_random_data():
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-
-
-if __name__ == "__main__":
-    for item in data:
 
 import random
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
 def generate_random_data():
-    main()
-import random
-
-    return data
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    for item in data:
-
-def main():    data = generate_random_data()
-    for item in data:
-
-
 
 def main():
-if __name__ == "__main__":
-def generate_random_data():
+    main()
+    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
     return data
-import random
+
+if __name__ == "__main__":
+
         print(f"Random Number: {item}")
+    data = generate_random_data()def generate_random_data():
+import random
     main()
+
+def main():
+    return data
+
+
+
+    data = generate_random_data()
+    for item in data:
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
